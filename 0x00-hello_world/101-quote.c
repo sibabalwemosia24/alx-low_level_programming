56a450e0a04ffff44d9e37812469fe8278c201d2
sibabalwemosia24 <104764538+sibabalwemosia24@users.noreply.github.com>
2022-06-07 01:41:38 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main- prints and that peice of art is useful
  *
- * Return: returns to 0
+ * Return: returns to 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19'));
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
