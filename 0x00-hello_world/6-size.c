b71b1b369fb09f9abdfb31279242a5461a23d191
sibabalwemosia24 <104764538+sibabalwemosia24@users.noreply.github.com>
2022-06-06 23:53:04 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main- prints the size of various types on the computer it is compiled and run on
+ * main- prints the size of types on the computer it is compiled and run on
  * Return: return to 0
  */
 int main(void)
