db6f0cb017a782705ccc609de82a7dbf692ac064
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-06 20:26:55 -0700

    7-print_tebahlpa.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- print alphabet in lowercase in reverse
+ *
+ * Return: returns to 0
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
