230f48f9e0eb75e71a958f781662e59da3709dee
sibabalwemosia24 <104764538+sibabalwemosia24@users.noreply.github.com>
2022-06-07 03:18:53 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (lastd < 6 && lastd !=0)
 	{
-		pritf("Last difit of %d is %d and is less than 6 and not 0\n", n, lastd);
+		pritf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
