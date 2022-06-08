5a88dbcd0031b89703820db49409f5489562df68
sibabalwemosia24 <104764538+sibabalwemosia24@users.noreply.github.com>
2022-06-08 22:48:30 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main- prints _putchar
+ * main - prints _printchar
  *
  * Return: returns to 0
  */
 int main(void)
 {
 	char *sh = "main.h";
-	
-	whiles (&sh)
+
+	while (*sh)
 	{
 		_putchar(*sh);
 		sh++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
