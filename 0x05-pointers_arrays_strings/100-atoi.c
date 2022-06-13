ddbb38a9ae7adabbad04ff6af42a4cfa0e342a5a
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-13 11:15:37 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count), '0' || *(s + count) > '9'))
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 		if (*(s + count) == '-')
 			pn *= -1;
@@ -23,7 +23,7 @@ int _atoi(char *s)
 			size++;
 		}
 	}
-	for (i = count - sizr; i < count; i++)
+	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
