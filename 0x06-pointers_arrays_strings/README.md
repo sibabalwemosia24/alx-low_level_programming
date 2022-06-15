812a7e3d3b6225d1674a954940b6edcbc0942217
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-15 07:35:24 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c - concatenates two strings
 1-strncat.c- concatenates two strings
 2-strncpy.c- copies string
+3-strcmp.c - compares two strings
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@ char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *strncat(char *desti, char *src, int n)i;
 char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
