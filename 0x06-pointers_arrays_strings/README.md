cf330299100d320e033a26688560d61facc4aec6
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-15 07:15:18 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-strcat.c - concatenates two strings
 1-strncat.c- concatenates two strings
+2-strncpy.c- copies string
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
-char *strncat(char *desti, char *src, int n);
+char *strncat(char *desti, char *src, int n)i;
+char *strncpy(char *dest, char *src, int n);
+
