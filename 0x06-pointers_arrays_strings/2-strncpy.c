58265c7e6c8176ef144a1fb1aabf7cfa75133c56
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-16 03:37:21 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies the string
+ * @dest: array that returns
+ * @src: the pointer that receives the string
+ * @n: times to interratet
+ * Return: the paremeter dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c- concatenates two strings
 1-strncat.c - concatenates two strings
+2-strncpy.c- copies a string
 
