51d51d8026989f491b0ae326a661a4c6a4b0f4a8
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-16 04:25:13 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: an array of intergers
+ * @n: number of element of an array
+ * Return: no return
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	i = 0;
+	n = n - 1;
+
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 1-strncat.c - concatenates two strings
 2-strncpy.c- copies a string
 3-strcmp.c - compars two strings
+4-rev_array.c - reverses array of intergers
 
