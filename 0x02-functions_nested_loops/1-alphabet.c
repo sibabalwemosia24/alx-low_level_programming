c28150df21c11541ceabc49e10508d5e7008834c
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-17 10:07:24 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet- print alphabet in lowercase
- *
+ * print_alphabet - print alphabet
  * Return: void
  */
+
 void print_alphabet(void)
 {
-	char c;
+	char letter = 'a';
 
-	for (c = 'a'; c ,+ 'z'; c ++ )
+	while (letter <= 'z')
 	{
-		_putchar(c);
+		_putchar(letter);
+		letter++;
 	}
-
 	_putchar('\n');
 }
