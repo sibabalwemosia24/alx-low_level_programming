c66722155fbafbe4d7e79c42a8bb5983907204e0
sibabalwemosia24 <sibabalwemosia@gmail.com>
2022-06-17 10:51:56 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ * @n: n is an interger
+ * Return: interger
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
