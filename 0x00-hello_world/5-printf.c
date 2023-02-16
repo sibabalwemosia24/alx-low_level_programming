c8633532a92218b2ed6847ae328d1d69fef5014a
sibabalwemosia24 <sibabalwemosia@gmail.com>
2023-02-16 02:46:57 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Description: prints 'with proper grammer, but the outcome is a piece of art,'
- * 
+ * Description: prints "with proper grammar, but the outcome is a piece
+ * of art,"
  * Return: Always 0 (Success)
  */
 int main(void)
