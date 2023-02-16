1d79e4b9ce6a9dbe74268f8c67ffc5824cf3c43d
sibabalwemosia24 <sibabalwemosia@gmail.com>
2023-02-16 02:30:33 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzles");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -2,3 +2,4 @@
 1- Compiles a C file but does not link
 2- Generates the assembly of a C code and saves it in an output file
 3- Compiles a C file and creates an executable names cisfun
+4- C programs that prints exactly "Programming is like building a multilingual puzzle"
