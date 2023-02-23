1c36b438cacef0e8ca568a3389c24d3c612a583f
sibabalwemosia24 <sibabalwemosia@gmail.com>
2023-02-23 13:08:55 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			if (x >= 10)
+				_putchat((x / 10) + 48);
+			_putchar((x % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 2. Function multiplies two integers
 3. Function prints numbers 0 to 9
 4. Function prints numbers from 0 to 9 accept 2 and 4
+5. Function prints 10 times the number 0 to 14
