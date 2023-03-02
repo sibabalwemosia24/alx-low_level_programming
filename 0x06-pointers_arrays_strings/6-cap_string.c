73cd61d1036ca92d39ac56680148ae2db8416f96
sibabalwemosia24 <sibabalwemosia@gmail.com>
2023-03-02 05:11:01 -0800

    corrected cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,30 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string
- * @str: the string to change first lettters of a word to uppercase
- * Return: capitalized letters
- *
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *str)
+
+char *cap_string(char *s)
 {
-	int i;
-	int j;
-	char c[] = {44, 57, 56, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	i = 0;
-
-	while (str[i] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+		for (i = 0; i < 13; i++)
 		{
-			str[i] = str[i] - 32;
-		}
-		j = 0;
-		while (c[j] != '\0')
-
-		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			if (*(s + count) == sep_words[i])
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
-			j++;
 		}
-		j++;
+		count++;
 	}
-	return (0);
+	return (s);
 }
